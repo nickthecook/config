6261ef5bc049f3610da931ed6c754c0beffb7395
nickthecook <nickthecook@gmail.com>
2024-06-08 07:24:15 -0400

    add installation script

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # config
 
-This repo contains the config for my shell environment and (todo) a script to install it on a Linux or Mac system.
+This repo contains the config for my shell environment and an idempotent script to install it on a Linux or Mac system.
+
+## Installation
+
+Run `./install.sh`. It will:
+
+1. backup existing files in their usual directories, e.g. move `$HOME/.zshrc` to `$HOME/.zshrc.bak`
+2. link the repository file into the original file's place
+
+If an error occurs backing up the file, it skips the file.
+
+If an error occurs linking the repo file into its place, it restores the backed-up file.
 
 ## Tools
 
---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+# for each file in this repo (other than this one and README)
+#   move any existing file under the same relative path under the home dir out of the way
+#   link the repo file into its place
+
+for file in `find -type f | grep -v '^\./\.git' | sed 's:^\./::g'`; do
+  [ "$file" == "README.md" -o "$file" == "install.sh" ] && continue
+
+  home_path="$HOME/$file"
+  repo_path="$PWD/$file"
+  home_dir=$(dirname "$home_path")
+  if [ -f "$home_path" ]; then
+    mv "$home_path" "$home_path.bak" || {
+      echo 1>&2 "$0: Failed to move '$home_path' to '$home_path.bak'; skipping "$file"..."
+      continue
+    }
+
+    echo "$0: Backed up '$home_path'."
+  elif [ ! -d "$home_dir" ]; then
+    mkdir -p "$home_dir" || {
+      echo 1>&2 "$0: Failed to create directory '$home_dir'; skipping '$file'..."
+      continue
+    }
+  fi
+
+  ln -sf "$repo_path" "$home_path" || {
+    echo 1>&2 "$0: Failed to link '$repo_path' to '$home_path'; skipping '$file'..."
+
+    if [ -f "$home_path.bak" ]; then
+      mv "$home_path.bak" "$home_path" || {
+        echo 1>&2 "$0: Failed to RESTORE '$home_path.bak'! Please manually restore the file to '$home_path'. Aborting..."
+        exit 2
+      }
+
+      echo 1>&2 "$0: Restored '$home_path'."
+      continue
+    fi
+  }
+
+  echo "$0: Linked '$file'"
+done
